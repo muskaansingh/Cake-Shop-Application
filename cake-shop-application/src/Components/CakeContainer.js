b5897c03f64079d97a40f0d62006deec918ac62f
Muskaan Singh <muskaansingh235@gmail.com>
2021-03-23 10:40:16 +0530

    adding cake action, state, reducer

---
+++ cake-shop-application/src/Components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+             <h1>Cake Shop Application</h1>
+             <button>Buy Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer
