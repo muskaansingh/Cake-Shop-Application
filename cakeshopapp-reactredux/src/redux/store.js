94aaeccb068734c6396a6cb744670661fce43e27
Muskaan Singh <muskaansingh235@gmail.com>
2021-04-05 16:23:19 +0530

    added middleware which adds logs on the console

--- cakeshopapp-reactredux/src/redux/store.js
+++ cakeshopapp-reactredux/src/redux/store.js
@@ -1,13 +1,17 @@
 // creating a store
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 // import Reducer from './cakes/cakeReducers';
 import rootReducer from './rootReducer';
+import logger from 'redux-logger';
 
 // const store = createStore(cakeReducer);
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
 
 // now provide this store to our react application 
 // (in App.js file) wrap the entire code in the <Provider> passing store as a parameter.
+
+
+//MIDDLEWARE: adds logs on the console
