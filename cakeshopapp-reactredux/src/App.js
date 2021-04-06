ca48f74c5e97bc734490fddf1612c2c9a2055c4b
Muskaan Singh <muskaansingh235@gmail.com>
2021-04-06 13:01:28 +0530

    render data on the UI

--- cakeshopapp-reactredux/src/App.js
+++ cakeshopapp-reactredux/src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import {Provider} from 'react-redux';
 import store from './redux/store';
-import CakeContainer from './Components/CakeContainer';
-import HooksCakeContainer from './Components/HooksCakeContainer';
-import IceCreamContainer from './Components/IceCreamContainer';
-import NewCakeContainer from './Components/NewCakeContainer';
-import ItemContainer from './Components/ItemContainer';
+import UserContainer from './Components/UserContainer';
+// import CakeContainer from './Components/CakeContainer';
+// import HooksCakeContainer from './Components/HooksCakeContainer';
+// import IceCreamContainer from './Components/IceCreamContainer';
+// import NewCakeContainer from './Components/NewCakeContainer';
+// import ItemContainer from './Components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-      <CakeContainer/>
-      <HooksCakeContainer/>
-      <IceCreamContainer/>
-      <hr/>
-      <NewCakeContainer/>
-    </div>
-    <hr/>
-    <hr/>
-    <ItemContainer cake/>
-    <ItemContainer/>
+        <div className="App">
+            {/* <CakeContainer/>
+            <HooksCakeContainer/>
+            <IceCreamContainer/>
+            <hr/>
+            <NewCakeContainer/>
+          
+            <hr/>
+            <hr/>
+            <ItemContainer cake/>
+            <ItemContainer/> */}
+
+          <UserContainer/>
+        </div>
     </Provider>
   );
 }
--- cakeshopapp-reactredux/src/Components/UserContainer.js
+++ cakeshopapp-reactredux/src/Components/UserContainer.js
@@ -1,13 +1,41 @@
 import React, {useEffect} from 'react';
-import connect from 'react-redux';
+import {connect} from 'react-redux';
+import {fetchUsers} from '../redux'
 
 
-function UserContainer() {
-    return (
-        <div>
-            <h1>USERS LIST</h1>
-        </div>
-    )
+function UserContainer({userData, fetchUsers}) {
+    useEffect(() => {
+        fetchUsers()
+    }, [ ])   // this empty dependency array is used to dispatch the fetchUsers action creator only once.
+
+    {/* render data to the UI */}
+
+    return  userData.loading ?
+             (<h2>Loading!</h2>) 
+            : userData.error ? 
+             (<h2>{userData.error}</h2>)
+            : (
+                <div>
+                    <h1>USER LIST</h1>
+                    <div>
+                        {userData && 
+                        userData.users &&
+                        userData.users.map(user => <p>{user.name}</p>)} 
+                    </div>
+                </div>
+        )
 }
 
-export default UserContainer;
+const mapStateToProps = (state) => {
+    return{
+        userData: state.user
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        fetchUsers: () => dispatch(fetchUsers())  // dispatch the fetchUsers action creator
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
